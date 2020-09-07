07915950da20bbe7743dbb73d278702ddf269dd4
Jose Palencia <joc.palencia@gmail.com>
2020-09-07 15:10:49 -0600

    Hola Mundo

---
+++ HolaMundo/HolaMundo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HolaMundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Buenas tardes, por favor ingrese su nombre");
+            string nombre = Console.ReadLine();
+
+            Console.WriteLine("Hola: " + nombre + " bienvenido.");
+
+            Console.ReadKey();
+        }
+    }
+}
