869347ae64afa5466decbcc2cd4edb46269d0c80
Jose Palencia <joc.palencia@gmail.com>
2020-09-07 22:16:10 -0600

    Trabajo con int y float

---
+++ Unidad 1/02-Numeros/02-Numeros/02-Numeros/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Numeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("----------------------");
+            Console.WriteLine(" Operaciones Basicas ");
+            Console.WriteLine("----------------------\n");
+
+            Console.WriteLine("Ingrese el primer numero:");
+            int numero1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el segundo numero:");
+            int numero2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nEl resultado de las operaciones es:");
+            Console.WriteLine("Suma: " + (numero1 + numero2));
+            Console.WriteLine("Resta: " + (numero1 - numero2));
+            Console.WriteLine("Multiplicacion: " + (numero1 * numero2));
+            Console.WriteLine("Division: " + (numero1 / numero2));
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ Unidad 1/02-Numeros/02-Numeros/02-Numeros/Venta.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Numeros
+{
+    class Venta
+    {
+
+        static void Main(string[] args) 
+        {
+            Console.WriteLine("--------------------------------");
+            Console.WriteLine("CALCULO DEL IMPUESTO SOBRE VENTA");
+            Console.WriteLine("--------------------------------\n");
+
+            Console.WriteLine("Ingrese el monto total de la venta:");
+            float valorTotal = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el el impuesto vigente:");
+            float porcentajeImpuesto = float.Parse(Console.ReadLine());
+
+            float impuestoPagar = valorTotal * porcentajeImpuesto;
+
+            Console.WriteLine("El valor total mas el impuesto es: " + (impuestoPagar+valorTotal));
+
+            Console.ReadKey();
+
+        }
+
+    }
+}
