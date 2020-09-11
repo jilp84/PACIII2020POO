dbf337fd3000b3e3865b568c246511d0c6435e86
Jose Palencia <joc.palencia@gmail.com>
2020-09-11 17:40:28 -0600

    Operador Ternario y Condicion If

---
+++ Unidad 1/03-Condicionales/03-Condicionales/CondicionIf.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_Condicionales
+{
+    class CondicionIf
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("------------------------------------");
+            Console.WriteLine("CONDICIONAL IF");
+            Console.WriteLine("------------------------------------\n");
+
+            Console.WriteLine("Numero 1:");
+            int numero1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Numero 2:");
+            int numero2 = int.Parse(Console.ReadLine());
+
+            if (numero1 > numero2)
+            {
+                Console.WriteLine("El primero numero es mayor");
+            }
+            else
+            {
+                Console.WriteLine("El primero numero es menor");
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Unidad 1/03-Condicionales/03-Condicionales/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_Condicionales
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("------------------------------------");
+            Console.WriteLine("COMPARAR NUMEROS - OPERADOR TERNARIO");
+            Console.WriteLine("------------------------------------\n");
+            
+            Console.WriteLine("Numero 1:");
+            int numero1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Numero 2:");
+            int numero2 = int.Parse(Console.ReadLine());
+
+            //string flag = numero1 == numero2 ? "Iguales" : "Distintos";
+
+            Console.WriteLine("Los numeros son: " + (numero1 == numero2 ? "Iguales" : "Distintos"));
+
+            Console.ReadKey();
+
+        }
+    }
+}
