9c35e025562330b44d3961cd527b7dbed45f25e7
Jose Palencia <joc.palencia@gmail.com>
2020-09-13 12:17:28 -0600

    Ciclo Do While

---
+++ Unidad 1/04-IteradoresBucles/04-IteradoresBucles/CicloDo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_IteradoresBucles
+{
+    class CicloDo
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("------------------");
+            Console.WriteLine("CICLO DO");
+            Console.WriteLine("------------------");
+
+            int i = 10;
+
+            do
+            {
+                Console.WriteLine(i);
+                i--;
+            } while (i >= 1);
+
+            Console.WriteLine("Presione una tecla para continuar");
+            Console.ReadKey();
+        }
+    }
+}
