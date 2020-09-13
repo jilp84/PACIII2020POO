2b52c783333416af05df9bdf4d1d377671b98ca3
Jose Palencia <joc.palencia@gmail.com>
2020-09-13 12:59:47 -0600

    Ciclo For

---
+++ Unidad 1/04-IteradoresBucles/04-IteradoresBucles/CicloFor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_IteradoresBucles
+{
+    class CicloFor
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("---------------------------");
+            Console.WriteLine("CICLO FOR");
+            Console.WriteLine("---------------------------");
+
+            for (int i = 10; i >= 1; i--) 
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine("Presione una tecla para continuar");
+            Console.ReadKey();
+
+        }
+    }
+}
