c38be36b872bcb67c1b0be8726f90a1afd95fe84
Jose Palencia <joc.palencia@gmail.com>
2020-09-13 12:45:50 -0600

    Ciclo While

---
+++ Unidad 1/04-IteradoresBucles/04-IteradoresBucles/CicloWhile.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_IteradoresBucles
+{
+    class CicloWhile
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-------------------");
+            Console.WriteLine("CICLO WHILE");
+            Console.WriteLine("-------------------");
+
+            int i = 1;
+
+            while (i <= 10)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+
+            Console.WriteLine("Presione una tecla para continuar");
+            Console.ReadKey();
+
+        }
+
+    }
+}
