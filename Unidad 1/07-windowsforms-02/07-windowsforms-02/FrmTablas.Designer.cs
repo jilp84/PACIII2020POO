330f33e66ae5fd41dab31b5b8838dceeb692cc35
cnv_jpalencia <jpalencia@coopnuevavida.com>
2020-09-20 13:48:35 -0600

    Validaciones Windows Forms

---
+++ Unidad 1/07-windowsforms-02/07-windowsforms-02/FrmTablas.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace _07_windowsforms_02
+{
+    partial class FrmTablas
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnMostrar = new System.Windows.Forms.Button();
+            this.txtLimite = new System.Windows.Forms.TextBox();
+            this.lblLimite = new System.Windows.Forms.Label();
+            this.txtNumero = new System.Windows.Forms.TextBox();
+            this.lblNumero = new System.Windows.Forms.Label();
+            this.txtSalida = new System.Windows.Forms.RichTextBox();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.btnMostrar);
+            this.groupBox1.Controls.Add(this.txtLimite);
+            this.groupBox1.Controls.Add(this.lblLimite);
+            this.groupBox1.Controls.Add(this.txtNumero);
+            this.groupBox1.Controls.Add(this.lblNumero);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(486, 69);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // btnMostrar
+            // 
+            this.btnMostrar.Location = new System.Drawing.Point(392, 23);
+            this.btnMostrar.Name = "btnMostrar";
+            this.btnMostrar.Size = new System.Drawing.Size(75, 23);
+            this.btnMostrar.TabIndex = 4;
+            this.btnMostrar.Text = "Mostrar";
+            this.btnMostrar.UseVisualStyleBackColor = true;
+            this.btnMostrar.Click += new System.EventHandler(this.btnMostrar_Click);
+            // 
+            // txtLimite
+            // 
+            this.txtLimite.Location = new System.Drawing.Point(267, 24);
+            this.txtLimite.Name = "txtLimite";
+            this.txtLimite.Size = new System.Drawing.Size(100, 20);
+            this.txtLimite.TabIndex = 3;
+            this.txtLimite.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtLimite_KeyPress);
+            this.txtLimite.Validating += new System.ComponentModel.CancelEventHandler(this.txtLimite_Validating);
+            this.txtLimite.Validated += new System.EventHandler(this.txtLimite_Validated);
+            // 
+            // lblLimite
+            // 
+            this.lblLimite.AutoSize = true;
+            this.lblLimite.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLimite.Location = new System.Drawing.Point(214, 26);
+            this.lblLimite.Name = "lblLimite";
+            this.lblLimite.Size = new System.Drawing.Size(53, 16);
+            this.lblLimite.TabIndex = 2;
+            this.lblLimite.Text = "Limite:";
+            // 
+            // txtNumero
+            // 
+            this.txtNumero.Location = new System.Drawing.Point(88, 24);
+            this.txtNumero.Name = "txtNumero";
+            this.txtNumero.Size = new System.Drawing.Size(100, 20);
+            this.txtNumero.TabIndex = 1;
+            this.txtNumero.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtNumero_KeyPress);
+            this.txtNumero.Validating += new System.ComponentModel.CancelEventHandler(this.txtNumero_Validating);
+            this.txtNumero.Validated += new System.EventHandler(this.txtNumero_Validated);
+            // 
+            // lblNumero
+            // 
+            this.lblNumero.AutoSize = true;
+            this.lblNumero.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNumero.Location = new System.Drawing.Point(20, 26);
+            this.lblNumero.Name = "lblNumero";
+            this.lblNumero.Size = new System.Drawing.Size(66, 16);
+            this.lblNumero.TabIndex = 0;
+            this.lblNumero.Text = "Numero:";
+            // 
+            // txtSalida
+            // 
+            this.txtSalida.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtSalida.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSalida.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.txtSalida.Location = new System.Drawing.Point(12, 87);
+            this.txtSalida.Name = "txtSalida";
+            this.txtSalida.Size = new System.Drawing.Size(486, 351);
+            this.txtSalida.TabIndex = 1;
+            this.txtSalida.Text = "";
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // FrmTablas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(510, 450);
+            this.Controls.Add(this.txtSalida);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "FrmTablas";
+            this.Text = "Tablas";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnMostrar;
+        private System.Windows.Forms.TextBox txtLimite;
+        private System.Windows.Forms.Label lblLimite;
+        private System.Windows.Forms.TextBox txtNumero;
+        private System.Windows.Forms.Label lblNumero;
+        private System.Windows.Forms.RichTextBox txtSalida;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+    }
+}
+
---
+++ Unidad 1/07-windowsforms-02/07-windowsforms-02/FrmTablas.cs
@@ -0,0 +1,93 @@
+﻿using _07_windowsforms_02.Utilidades;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _07_windowsforms_02
+{
+    public partial class FrmTablas : Form
+    {
+        public FrmTablas()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMostrar_Click(object sender, EventArgs e)
+        {
+            Calcular();
+
+        }
+
+        private void Calcular() 
+        {
+            int numero = int.Parse(txtNumero.Text);
+            int limite = int.Parse(txtLimite.Text);
+
+            txtSalida.Text = GetTabla(numero, limite);
+        }
+
+        private string GetTabla(int numero, int limite) 
+        {
+            string tabla = "";
+            for (int i = 1; i <= limite; i++)
+            {
+                tabla += String.Format("{0} x {1} = {2}\n", numero, i, (numero*i));
+            }
+
+            return tabla;
+        
+        }
+
+        private void txtNumero_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            ValidarTextbox.SoloNumeros(e);
+        }
+
+        private void txtLimite_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+            ValidarTextbox.SoloNumeros(e);
+            if ((int)e.KeyChar == (int)Keys.Enter && txtLimite.Text != "")
+            {
+                e.Handled = true;
+                Calcular();
+            }
+        }
+
+        private void txtNumero_Validating(object sender, CancelEventArgs e)
+        {
+            if (txtNumero.Text == "")
+            {
+                e.Cancel = true;
+                txtNumero.Select(0, txtNumero.Text.Length);
+                errorProvider1.SetError(txtNumero, "Debe ingresar un numero");
+            }
+        }
+
+        private void txtNumero_Validated(object sender, EventArgs e)
+        {
+            errorProvider1.SetError(txtNumero, "");
+        }
+
+        private void txtLimite_Validating(object sender, CancelEventArgs e)
+        {
+            if (txtLimite.Text == "")
+            {
+                e.Cancel = true;
+                txtLimite.Select(0, txtLimite.Text.Length);
+                errorProvider1.SetError(txtLimite, "Debe ingresar un numero");
+            }
+        }
+
+        private void txtLimite_Validated(object sender, EventArgs e)
+        {
+            errorProvider1.SetError(txtLimite, "");
+        }
+    }
+}
